33b91d812416282a9eb6fde38cc3def4b4f85772
bkvist <bkvist@hotmail.com>
2021-03-19 09:39:35 +0100

    test2

--- Opgaver/opgave12.2/delete.js
+++
@@ -1,9 +0,0 @@
-// delete.js
-async function deLete(url) {
-    let respons = await fetch(url, {
-        method: "DELETE"
-    });
-    if (respons.status !== 200) // OK
-        throw new Error(respons.status);
-    return await respons.json();
-}
